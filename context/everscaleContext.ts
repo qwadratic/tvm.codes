defa29d3bfe2d7159fee7de5577072dc3dded50b
i <ii@MacBook-Pro-i.local>
2023-09-04 14:14:27 +0200

    remove dependency from EVM where opcode is a number

--- context/everscaleContext.ts
+++ context/everscaleContext.ts
@@ -1,14 +1,48 @@
 import { TvmOpcodeStructure } from '../types/index'
 import React, { createContext, useEffect, useState, useRef } from 'react'
 import { ContextProps, initialExecutionState } from './ethereumContext'
-import { Opcode, OpcodeList } from '@ethereumjs/evm/src/opcodes'
+class Opcode {
+  readonly code: string
+  readonly name: string
+  readonly fullName: string
+  readonly fee: number
+  readonly isAsync: boolean
+  readonly dynamicGas: boolean
+
+  constructor({
+    code,
+    name,
+    fullName,
+    fee,
+    isAsync,
+    dynamicGas,
+  }: {
+    code: string
+    name: string
+    fullName: string
+    fee: number
+    isAsync: boolean
+    dynamicGas: boolean
+  }) {
+    this.code = code
+    this.name = name
+    this.fullName = fullName
+    this.fee = fee
+    this.isAsync = isAsync
+    this.dynamicGas = dynamicGas
+
+    // Opcode isn't subject to change, thus all further modifications are prevented.
+    Object.freeze(this)
+  }
+}
+type OpcodeList = Map<string, Opcode>
 const jsonData = require('../opcodes.json')
 export class OpcodeData {
   names: string[]
   aliasOfs: string[]
   tlbs: string[]
   Categories: string[]
-  opcodes: number[]
+  opcodes: string[]
   Fifts: string[]
   stacks: string[]
   inputs: string[]
@@ -24,7 +58,7 @@ export class OpcodeData {
     this.aliasOfs = tempJson.map((key) => key[1].alias_of)
     this.tlbs = tempJson.map((key) => key[1].tlb)
     this.Categories = tempJson.map((key) => key[1].category)
-    this.opcodes = tempJson.map((key) => parseInt(key[1].opcode))
+    this.opcodes = tempJson.map((key) => key[1].opcode)
     this.Fifts = tempJson.map((key) => key[1].fift)
     this.inputs = tempJson.map((key) => key[1].input)
     this.outputs = tempJson.map((key) => key[1].output)
@@ -45,7 +79,7 @@ export class OpcodeData {
         dynamicGas: false,
       })
       console.log(tempOpcode)
-      opcodes.set(index, tempOpcode)
+      opcodes.set(opcode, tempOpcode)
     })
     return opcodes
   }
